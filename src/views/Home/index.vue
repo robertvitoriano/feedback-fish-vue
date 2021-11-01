2c5293ed14a77433858f5fbdff7f7b2e2e833af6
Robert Vitoriano <robertvitoriano@gmail.com>
2021-11-01 20:41:53 -0300

    configure router

---
+++ src/views/Home/index.vue
@@ -0,0 +1,22 @@
+<template>
+<div>Aqui é a home</div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    },
+    methods: {
+        onClick: () => {
+          console.log('Hello World')
+        }
+        }
+}
+</script>
+
+<style>
+</style>
